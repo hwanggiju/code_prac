fd5ff1d74cf24ab93f89434ab2542be0bdd84b2b
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-17 19:42:20 +0900

    알람시계

--- practice.py
+++ practice.py
@@ -1,14 +1,20 @@
-x = int(input())
-y = int(input())
+H, M = map(int, input().split(' '))
 
-if (x > 0) & (y > 0) :
-    print(1)
+T = (H * 60) + M
+H_M = T - 45
+
+if H_M > 0 :
+    H = int(H_M / 60)
+    M = int(H_M % 60)
+    print(H, M)
     
-elif (x < 0) & (y > 0) :
-    print(2)
-    
-elif (x > 0) & (y < 0) :
-    print(4)
-    
+elif H_M < 0 :
+    H_M = 1440 + H_M
+    H = int(H_M / 60)
+    M = int(H_M % 60)
+    print(H, M)
+
 else :
-    print(3)
+    H = int(H_M / 60)
+    M = int(H_M % 60)
+    print(H, M)
