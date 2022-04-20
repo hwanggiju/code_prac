c1e9c734adcab983a6e3455aed0691bc5182b418
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-20 21:33:55 +0900

    소트인사이드

--- practice.py
+++ practice.py
@@ -1,46 +1,11 @@
 import sys
-from collections import Counter
-
 
 input = sys.stdin.readline
+result = ''
+value = list(input())
+value.remove('\n')
+value.sort(key=int, reverse=True)
+for i in value :
+    result += i
 
-num = int(input())
-value = []
-
-for i in range(num):
-    value.append(int(input()))
-
-value.sort()   
- 
-# ---------------------------------------------
-aver = 0
-for i in value:
-    aver += i
-aver /= num
-
-# ---------------------------------------------
-index = int(num / 2)
-medi = value[index]
-
-# ---------------------------------------------
-c = Counter(value)
-val = c.most_common()
-maximum = val[0][1]
-mode = []
-
-for i in val:
-    if i[1] == maximum:
-        mode.append(i[0])
-    else :
-        mode.append(val[0][0])
-        
-# ---------------------------------------------
-M, m = max(value), min(value)
-
-print(round(aver))
-print(medi)
-if len(mode) > 1 :
-    print(mode[1])
-else :
-    print(mode[0])
-print(M-m)
+print(result)
