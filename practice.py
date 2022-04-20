b56c8bcbbc87601d7e264f0d14bb36403f56ebe6
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-20 21:11:57 +0900

    통계학
    최빈값 -> collection 라이브러리 Counter 활용

--- practice.py
+++ practice.py
@@ -1,12 +1,46 @@
 import sys
+from collections import Counter
+
 
 input = sys.stdin.readline
-index = int(input())
-cnt = [0 for x in range(10001)]
 
-for _ in range(index) :
-    cnt[int(input())] += 1
-    
-for i in range(1, 10001) :
-    for j in range(cnt[i]) :
-        print(i)
+num = int(input())
+value = []
+
+for i in range(num):
+    value.append(int(input()))
+
+value.sort()   
+ 
+# ---------------------------------------------
+aver = 0
+for i in value:
+    aver += i
+aver /= num
+
+# ---------------------------------------------
+index = int(num / 2)
+medi = value[index]
+
+# ---------------------------------------------
+c = Counter(value)
+val = c.most_common()
+maximum = val[0][1]
+mode = []
+
+for i in val:
+    if i[1] == maximum:
+        mode.append(i[0])
+    else :
+        mode.append(val[0][0])
+        
+# ---------------------------------------------
+M, m = max(value), min(value)
+
+print(round(aver))
+print(medi)
+if len(mode) > 1 :
+    print(mode[1])
+else :
+    print(mode[0])
+print(M-m)
