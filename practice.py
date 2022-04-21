2ec00ccc05cde59a4c8dae136019b9daf4e9922a
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-21 22:02:02 +0900

    좌표 정렬하기2

--- practice.py
+++ practice.py
@@ -6,11 +6,20 @@ value = []
 
 for i in range(num):
     x, y = map(int, input().split(' '))
-    tup = (x, y)
-    value.append(tup)
+    x_y_list = [x, y]
+    value.append(x_y_list)
+    
+for i in value :
+    temp_1 = i[0]
+    i[0] = i[1]
+    i[1] = temp_1
     
 value.sort()
 
 for i in value :
+    temp_2 = i[0]
+    i[0] = i[1]
+    i[1] = temp_2
+
+for i in value :
     print(i[0], i[1])
-print(value)
