b97a0cf6f4a85f945882bef4c99ec1a3f0a99a33
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-21 21:54:20 +0900

    좌표 정렬하기

--- practice.py
+++ practice.py
@@ -1,11 +1,16 @@
 import sys
 
 input = sys.stdin.readline
-result = ''
-value = list(input())
-value.remove('\n')
-value.sort(key=int, reverse=True)
-for i in value :
-    result += i
+num = int(input())
+value = []
 
-print(result)
+for i in range(num):
+    x, y = map(int, input().split(' '))
+    tup = (x, y)
+    value.append(tup)
+    
+value.sort()
+
+for i in value :
+    print(i[0], i[1])
+print(value)
