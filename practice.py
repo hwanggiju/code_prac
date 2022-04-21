d78497b76570f8f2b66fd7db657f0d012355cd69
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-21 22:49:36 +0900

    나이 순 정렬
    sorted 함수

--- practice.py
+++ practice.py
@@ -1,25 +1,16 @@
 import sys
 
 input = sys.stdin.readline
-num = int(input())
-value = []
 
-for i in range(num):
-    x, y = map(int, input().split(' '))
-    x_y_list = [x, y]
-    value.append(x_y_list)
+person = int(input())
+lst = [0 for _ in range(person)]
+
+for i in range(person) :
+    age, name = map(str, input().split(' '))
+    lst[i] = [int(age), name]
+    lst[i][1] = lst[i][1].rstrip('\n')
     
-for i in value :
-    temp_1 = i[0]
-    i[0] = i[1]
-    i[1] = temp_1
-    
-value.sort()
+lst = sorted(lst, key = lambda x : x[0])
 
-for i in value :
-    temp_2 = i[0]
-    i[0] = i[1]
-    i[1] = temp_2
-
-for i in value :
-    print(i[0], i[1])
+for i in lst :
+    print(i[0], i[1])    
