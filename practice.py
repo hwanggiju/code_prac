5a9fe46813a54a0ad03e4275999607adae450849
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-04-25 16:19:22 +0900

    피보나치 수 5

--- practice.py
+++ practice.py
@@ -3,18 +3,28 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-result = 1
-cnt = 0
-def fac(result, cnt) :
-    result *= (cnt+1)
-    cnt += 1
-    if cnt == N :
-        return print(result)
-    
-    elif N == 0 :
-        return print(result)
+pre = 0
+post = 1
+i = 0
+def fun(i, pre, post):
+    if N == 0 :
+        return print(pre)
+        
+    if ((i%2) == 0) : 
+        pre += post
+        i += 1
     
     else :
-        fac(result, cnt)
-
-fac(result, cnt)
+        post += pre
+        i += 1
+        
+    if (i == N) & (i%2 == 0) :
+        return print(pre)
+    
+    elif (i == N) & (i%2 != 0) :
+        return print(post)
+    
+    else :
+        fun(i, pre, post)
+        
+fun(i, pre, post)
