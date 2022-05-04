ee97625d4a9210a5d723e8421086c73fd186f3c2
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-04 15:07:13 +0900

    재귀함수가 뭔가요?

--- practice.py
+++ practice.py
@@ -2,15 +2,41 @@ import sys
 
 input = sys.stdin.readline
 
-def f(N, start, end) :
-    if N == 1:
-        print(start, end)
+N = int(input())
+print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+
+Q = '\"재귀함수가 뭔가요?\"'
+
+answer1 = '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+answer2 = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
+answer3 = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"'
+
+answer4 = '\"재귀함수는 자기 자신을 호출하는 함수라네\"'
+
+ending = '라고 답변하였지.'
+
+underbar = '____'
+
+def f(start):
+    if start == N :
+        print(underbar*start + Q)
+        print(underbar*start + answer4)
+        for i in range(N+1) :
+            print(underbar*(N-i) + ending)
         return
     
-    f(N - 1, start, 6 - start - end)
-    print(start, end)
-    f(N - 1, 6 - start - end, end)
+    if start == 0 :
+        print(Q)
+        print(answer1)
+        print(answer2)
+        print(answer3)
+        
+    if start >= 1:
+        print(underbar*start + Q)
+        print(underbar*start + answer1)
+        print(underbar*start + answer2)
+        print(underbar*start + answer3)
+    f(start + 1)
 
-N = int(input()) # 원판개수
-print(2**N - 1)
-f(N, 1, 3)
+start = 0
+f(start)
