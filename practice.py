1efbfeba58bf2422cfd5a3e0759a70309aa691f1
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-06 20:40:29 +0900

    피보나치 함수

--- practice.py
+++ practice.py
@@ -2,41 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+T = int(input())
+cnt = [[0, 0] for i in range(41)]
+tc = [int(input()) for i in range(T)]
 
-Q = '\"재귀함수가 뭔가요?\"'
+cnt[0] = [1, 0]
+cnt[1] = [0, 1]
 
-answer1 = '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
-answer2 = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
-answer3 = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"'
+def fibo(n) :
+    if cnt[n] == [0, 0] :
+        cnt[n] = [x+y for x,y in zip(fibo(n-1), fibo(n-2))]
+        return cnt[n]
 
-answer4 = '\"재귀함수는 자기 자신을 호출하는 함수라네\"'
+    else :
+        return cnt[n]
 
-ending = '라고 답변하였지.'
-
-underbar = '____'
-
-def f(start):
-    if start == N :
-        print(underbar*start + Q)
-        print(underbar*start + answer4)
-        for i in range(N+1) :
-            print(underbar*(N-i) + ending)
-        return
-    
-    if start == 0 :
-        print(Q)
-        print(answer1)
-        print(answer2)
-        print(answer3)
-        
-    if start >= 1:
-        print(underbar*start + Q)
-        print(underbar*start + answer1)
-        print(underbar*start + answer2)
-        print(underbar*start + answer3)
-    f(start + 1)
-
-start = 0
-f(start)
+for i in tc :
+    fibo(i)
+    print(*cnt[i])
