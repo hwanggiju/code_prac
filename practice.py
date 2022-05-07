e5666942b64afdce6e7e3a83184c43d62757815f
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-07 16:50:32 +0900

    신나는 함수 실행

--- practice.py
+++ practice.py
@@ -1,22 +1,23 @@
 import sys
 
 input = sys.stdin.readline
+lst = [[[0]*(21) for _ in range(21)] for _ in range(21)]
 
-T = int(input())
-cnt = [[0, 0] for i in range(41)]
-tc = [int(input()) for i in range(T)]
+def w(a, b, c) :
+    if a <= 0 or b <= 0 or c <= 0 : 
+        return 1
+    if a > 20 or b >20 or c > 20 :
+        return w(20, 20, 20)
+    if lst[a][b][c] :
+        return lst[a][b][c]
+    if a < b < c :
+        lst[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return lst[a][b][c]
+    lst[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return lst[a][b][c]
 
-cnt[0] = [1, 0]
-cnt[1] = [0, 1]
-
-def fibo(n) :
-    if cnt[n] == [0, 0] :
-        cnt[n] = [x+y for x,y in zip(fibo(n-1), fibo(n-2))]
-        return cnt[n]
-
-    else :
-        return cnt[n]
-
-for i in tc :
-    fibo(i)
-    print(*cnt[i])
+while True :
+    a, b, c = map(int, input().split(' '))
+    if a == -1 and b == -1 and c == -1 :
+        break
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
