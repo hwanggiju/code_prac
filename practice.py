44ab496b043b9dffbac36a351fac0931a7a4c9de
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-08 16:38:20 +0900

    01 타일

--- practice.py
+++ practice.py
@@ -1,23 +1,20 @@
 import sys
 
 input = sys.stdin.readline
-lst = [[[0]*(21) for _ in range(21)] for _ in range(21)]
 
-def w(a, b, c) :
-    if a <= 0 or b <= 0 or c <= 0 : 
-        return 1
-    if a > 20 or b >20 or c > 20 :
-        return w(20, 20, 20)
-    if lst[a][b][c] :
-        return lst[a][b][c]
-    if a < b < c :
-        lst[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-        return lst[a][b][c]
-    lst[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-    return lst[a][b][c]
+N = int(input())
 
-while True :
-    a, b, c = map(int, input().split(' '))
-    if a == -1 and b == -1 and c == -1 :
-        break
-    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+dp = [0] * 1000001
+
+dp[1] = 1
+dp[2] = 2
+
+def f(n) :
+    if dp[n] == 0 :
+        dp[n] = (dp[n-1] + dp[n-2]) % 15746
+        return dp[n]
+
+for i in range(3, N + 1) :
+    f(i)
+
+print(dp[N])
