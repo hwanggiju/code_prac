9a11e4194bf8e1401d8ab922afb8cf93700424ce
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-08 21:54:22 +0900

    파도반 수열

--- practice.py
+++ practice.py
@@ -2,19 +2,25 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
+T = int(input())
+Tc = []
 
-dp = [0] * 1000001
+for i in range(T) :
+    Tc.append(int(input()))
 
+dp = [0] * 101
 dp[1] = 1
-dp[2] = 2
-
-def f(n) :
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+def P(n) :
+    if n == 5 :
+        dp[n] = dp[n-1]
     if dp[n] == 0 :
-        dp[n] = (dp[n-1] + dp[n-2]) % 15746
-        return dp[n]
+        dp[n] = dp[n-5] + dp[n-1]
+    return dp[n]
 
-for i in range(3, N + 1) :
-    f(i)
-
-print(dp[N])
+for i in Tc :
+    for j in range(4, i+1) :
+        P(j)
+    print(dp[i])
