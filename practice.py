6f8d501fe99d1aa10c42e2274fb4617ff01094ba
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-13 15:30:54 +0900

    정수 삼각형

--- practice.py
+++ practice.py
@@ -2,15 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 dp = []
 
-for i in range(N) :
+for i in range(n) :
     dp.append(list(map(int, input().split())))
 
-for i in range(1, N) :
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
-
-print(min(dp[N-1][0], dp[N-1][1], dp[N-1][2]))
+for i in range(1, n) :
+    dp[i][0] = dp[i][0] + dp[i-1][0]
+    dp[i][len(dp[i])-1] = dp[i-1][len(dp[i-1])-1] + dp[i][len(dp[i])-1]
+    
+    if len(dp[i]) >= 3:
+        for j in range(1, (len(dp[i]) - 1)) :
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + dp[i][j]
+            
+print(max(dp[n-1]))
