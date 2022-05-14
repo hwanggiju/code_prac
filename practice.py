d7a4ba27141b535dd66d6bdee22f928b51f9909d
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-14 18:06:38 +0900

    1로 만들기

--- practice.py
+++ practice.py
@@ -2,19 +2,15 @@ import sys
 
 input = sys.stdin.readline
 
-step = int(input())
-score = [0 for i in range(301)]
-dp = [0  for i in range(301)]
+N = int(input())
+dp = [0 for i in range(N+1)]
 
-for i in range(step) :
-    score[i] = int(input())
+for i in range(2, N+1) :
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0 :
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0 :
+        dp[i] = min(dp[i], dp[i//2] + 1)
 
-dp[0] = score[0]
-dp[1] = score[0] + score[1]
-dp[2] = max(score[2] + score[1], score[2] + score[0]) 
-
-for i in range(3, step) :
-    dp[i] = max(dp[i-3] + score[i-1] + score[i], 
-                score[i] + dp[i-2])
-    
-print(dp[step-1])
+print(dp[N])   
+        
