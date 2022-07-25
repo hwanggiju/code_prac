508a6e6fa883861066600d22b79758fee5b127de
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-25 15:25:13 +0900

    24416번 문제
    This reverts commit a99c843665285648808b5eee897bb165f656436b.

--- practice.py
+++ practice.py
@@ -1,23 +1,16 @@
-# 백준 24416 문제 풀이
 import sys
 
-def fib(n) :
-    if (n == 1 or n == 2) :
-        return 1
-    
-    else :
-        return (fib(n-1) + fib(n-2)) 
-    
-def fibonacci(n) :
-    dp = [0] * (n+1) 
-    dp[1] = 1
-    dp[2] = 1
-    cnt = 0
-    for i in range(3, n + 1) :
-        cnt += 1
-        dp[i] = dp[i-1] + dp[i-2]
-    return cnt
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+N = int(input())
+dp = [0 for i in range(N+1)]
 
-print(fib(n), fibonacci(n))
+for i in range(2, N+1) :
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0 :
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0 :
+        dp[i] = min(dp[i], dp[i//2] + 1)
+
+print(dp[N])   
+        
