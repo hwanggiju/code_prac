58bb93f7b68312b42f8a6d49f4a601f64760bf8c
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-25 17:16:12 +0900

    1912번 문제 풀이

--- practice.py
+++ practice.py
@@ -1,22 +1,13 @@
 import sys
 
-def fib(n) :
-    if (n == 1 or n == 2) :
-        return 1
-    
-    else :
-        return (fib(n-1) + fib(n-2)) 
-    
-def fibonacci(n) :
-    dp = [0] * (n+1) 
-    dp[1] = 1
-    dp[2] = 1
-    cnt = 0
-    for i in range(3, n + 1) :
-        cnt += 1
-        dp[i] = dp[i-1] + dp[i-2]
-    return cnt
+n = int(input())
 
-n = int(sys.stdin.readline())
+lst = list(map(int, input().split()))
 
-print(fib(n), fibonacci(n))
+dp = [0] * n
+dp[0] = lst[0]
+
+for i in range(1, n) :
+    dp[i] = max(lst[i], dp[i-1]+lst[i])
+
+print(max(dp))
