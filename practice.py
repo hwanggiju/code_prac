e8f0ad36a2654d9c48b4da0da47c4e629876e2cd
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-25 15:26:29 +0900

    24416번 문제 풀이

--- practice.py
+++ practice.py
@@ -1,16 +1,22 @@
 import sys
 
-input = sys.stdin.readline
+def fib(n) :
+    if (n == 1 or n == 2) :
+        return 1
+    
+    else :
+        return (fib(n-1) + fib(n-2)) 
+    
+def fibonacci(n) :
+    dp = [0] * (n+1) 
+    dp[1] = 1
+    dp[2] = 1
+    cnt = 0
+    for i in range(3, n + 1) :
+        cnt += 1
+        dp[i] = dp[i-1] + dp[i-2]
+    return cnt
 
-N = int(input())
-dp = [0 for i in range(N+1)]
+n = int(sys.stdin.readline())
 
-for i in range(2, N+1) :
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0 :
-        dp[i] = min(dp[i], dp[i//3] + 1)
-    if i % 2 == 0 :
-        dp[i] = min(dp[i], dp[i//2] + 1)
-
-print(dp[N])   
-        
+print(fib(n), fibonacci(n))
