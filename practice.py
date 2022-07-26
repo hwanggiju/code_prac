1fc665d03830d52773a8db5a2f6b158def73a30c
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-26 13:11:09 +0900

    10844번 문제 풀이

--- practice.py
+++ practice.py
@@ -1,13 +1,22 @@
 import sys
 
-n = int(input())
+N = int(input())
+dp = [[0 for i in range(10)] for j in range(101)]
 
-lst = list(map(int, input().split()))
+# 1, 2, 3, 4, 5, 6, 7, 8, 9
+# 1 2, 2 1, 2 3, 3 2, 3 4, 4 3, 4 5, 5 4, 5 6, 6 5, 6 7,
+# 7 6, 7 8, 8 7, 8 9, 9 8
+for i in range(1, 10) :
+    dp[1][i] = 1
 
-dp = [0] * n
-dp[0] = lst[0]
-
-for i in range(1, n) :
-    dp[i] = max(lst[i], dp[i-1]+lst[i])
-
-print(max(dp))
+for i in range(2, N+1) :
+    for j in range(10) :
+        if j == 0 :
+            dp[i][j] = dp[i-1][1]
+        elif j == 9 :
+            dp[i][j] = dp[i-1][8]
+        else :
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+print(dp)            
+print(sum(dp[N]) % 1000000000)
+    
