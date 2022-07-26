ae2c21122bfc2b7e89aefef1902cf9bd9db788e7
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-26 13:38:07 +0900

    modify

--- practice.py
+++ practice.py
@@ -3,9 +3,6 @@ import sys
 N = int(input())
 dp = [[0 for i in range(10)] for j in range(101)]
 
-# 1, 2, 3, 4, 5, 6, 7, 8, 9
-# 1 2, 2 1, 2 3, 3 2, 3 4, 4 3, 4 5, 5 4, 5 6, 6 5, 6 7,
-# 7 6, 7 8, 8 7, 8 9, 9 8
 for i in range(1, 10) :
     dp[1][i] = 1
 
@@ -17,6 +14,5 @@ for i in range(2, N+1) :
             dp[i][j] = dp[i-1][8]
         else :
             dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-print(dp)            
-print(sum(dp[N]) % 1000000000)
     
+print(sum(dp[N]) % 1000000000)
