45f9d06753cf70f26ae4138e669f418c18f891aa
hwanggiju <qltjzj555@gmail.com>
2022-12-14 17:41:14 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,18 +1,8 @@
-import sys
-
 N = int(input())
-dp = [[0 for i in range(10)] for j in range(101)]
-
-for i in range(1, 10) :
-    dp[1][i] = 1
-
-for i in range(2, N+1) :
-    for j in range(10) :
-        if j == 0 :
-            dp[i][j] = dp[i-1][1]
-        elif j == 9 :
-            dp[i][j] = dp[i-1][8]
-        else :
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-    
-print(sum(dp[N]) % 1000000000)
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+for i in range(3, len(dp)) :
+    dp[i] = dp[i-2] + dp[i-1]
+        
+print(dp[N] % 10007)
