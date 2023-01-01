38e08cf90680ed9b23edf98e97135c7823101593
hwanggiju <qltjzj555@gmail.com>
2023-01-01 14:46:39 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,23 +1,18 @@
 def solution(storey):
     answer = 0
-    tmp = 0
     while storey :
-        
-        b = storey % 10 # 나머지
+        b = storey % 10
         n = (storey // 10) % 10
         
         if b > 5 :
             answer += 10 - b
-            storey += 10
-            
+            storey += 10  
         elif b == 5 :
             answer += b
             if n >= 5 :
                 storey += 10
-            
         else :
             answer += b
             
-        storey = storey // 10 # 몫
-    
+        storey = storey // 10
     return answer
