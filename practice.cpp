cf36e8bb01adf3c88060df66d7e36d60a43ef7cf
hwanggiju <qltjzj555@gmail.com>
2023-01-05 17:42:44 +0900

    디펜스 게임 c++

--- practice.cpp
+++ practice.cpp
@@ -1,47 +1,34 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int col_tmp;
-
-bool cmp (vector<int>& x, vector<int>& y) {
-    if (x[col_tmp - 1] < y[col_tmp - 1])
-        return true;
-    
-    if (x[col_tmp - 1] > y[col_tmp - 1])
-        return false;
-    
-    if (x[col_tmp - 1] == y[col_tmp - 1]) {
-        if (x[0] > y[0])
-            return true;
-        else
-            return false;
-    }
-}
-
-int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+int solution(int n, int k, vector<int> enemy) {
     int answer = 0;
-    int tmp = 0;
-    col_tmp = col;
+    int sum = 0;
+    priority_queue<int, vector<int>> pq;
     
-    sort(data.begin(), data.end(), cmp);
-    
-    for (int i = row_begin - 1; i < row_end; i++) {
-        int sum = 0;
+    for (int i = 0; i < enemy.size(); i++) {
         
-        for (int j = 0; j < data[i].size(); j++) {
-            sum += (data[i][j] % row_begin);
+        sum += enemy[i];
+        
+        if (sum <= n) {
+            pq.push(enemy[i]);
+            answer += 1;
         }
-        if (tmp == 0)
-            tmp = sum;
-        else tmp = tmp ^ sum;
         
-        row_begin += 1;
+        else if (k > 0) {
+            k -= 1;
+            pq.push(enemy[i]);
+            int tmp = pq.top();
+            pq.pop();
+            sum -= tmp;
+            answer += 1;
+        }
+            
+        else break;
     }
-    
-    answer = tmp;
     
     return answer;
 }
