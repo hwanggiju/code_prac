fbe7eaf617f0e985a483cc3116180e6814e343ea
hwanggiju <qltjzj555@gmail.com>
2023-02-02 19:07:03 +0900

    프로그래머스 / 뒤에 있는 큰 수 찾기 C++

--- practice.cpp
+++ practice.cpp
@@ -1,83 +1,30 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <stack>
 
 using namespace std;
 
-int solution(int x, int y, int n) {
-    int answer = 1;
-    vector<int> nodeLst;
+vector<int> solution(vector<int> numbers) {
+    int n = numbers.size();
     
-    if (x == y) {
-        return 0;
-    }
+    vector<int> answer(n, -1);
+    stack<int> s;
     
-    int a = x + n;
-    int b = x * 2;
-    int c = x * 3;
-    
-    if (a < y) {
-        nodeLst.push_back(a);
-    }
-    else if (a == y) {
-        return answer;
-    }
-    
-    if (b < y) {
-        nodeLst.push_back(b);
-    }
-    else if (b == y) {
-        return answer;
-    }
-    
-    if (c < y) {
-        nodeLst.push_back(c);
-    }
-    else if (c == y) {
-        return answer;
-    }
-    
-    while (true) {
-        answer += 1;
-        vector<int> newnodeLst;
+    for (int i = 0; i < n; i++) {
+
+        int tmp = numbers[i];
         
-        for(int i = 0; i < nodeLst.size(); i++) {
-            int _plus = nodeLst[i] + n;
-            int _dou = nodeLst[i] * 2;
-            int _tri = nodeLst[i] * 3;
-            
-            if (_plus < y) {
-                newnodeLst.push_back(_plus);
+        while (!s.empty()) {
+            if (numbers[s.top()] < tmp) {
+                answer[s.top()] = tmp;
+                s.pop();
             }
-            else if (_plus == y) {
-                return answer;
-            }
-            
-            if (_dou < y) {
-                newnodeLst.push_back(_dou);
-            }
-            else if (_dou == y) {
-                return answer;
-            }
-            
-            if (_tri < y) {
-                newnodeLst.push_back(_tri);
-            }
-            else if (_tri == y) {
-                return answer;
+            else {
+                break;
             }
         }
         
-        if (newnodeLst.size() == 0) {
-            return -1;
-        }
-        
-        sort(newnodeLst.begin(), newnodeLst.end());
-        newnodeLst.erase(unique(newnodeLst.begin(), newnodeLst.end()), newnodeLst.end());
-        nodeLst = newnodeLst;
-        
+        s.push(i);
     }
-    
-    
     return answer;
 }
