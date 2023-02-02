81412cbab038195e7d49c1f322029a97a96d568f
hwanggiju <qltjzj555@gmail.com>
2023-02-02 12:57:15 +0900

    프로그래머스 / 뒤에 있는 큰 수 찾기 Python

--- practice.py
+++ practice.py
@@ -1,63 +1,19 @@
-def plus(x, n) :
-    return x + n
-def double_(x) :
-    return x * 2
-def triple(x) :
-    return x * 3
-
-def solution(x, y, n):
-    answer = 1
-    nodeLst = []
+from heapq import *
+def solution(numbers):
+    answer = [-1] * len(numbers)
     
-    if x == y :
-        return 0
+    lst = []
     
-    a = plus(x, n)
-    b = double_(x)
-    c = triple(x)
-    
-    if a < y :
-        nodeLst.append(a)
-    elif a == y :
-        return answer
-    
-    if b < y :
-        nodeLst.append(b)
-    elif b == y :
-        return answer
-    
-    if c < y :
-        nodeLst.append(c)
-    elif c == y :
-        return answer
-    
-    while True :
-        newnodeLst = []
-        answer += 1
+    for i in range(len(numbers)) :
+        tmp = numbers[i]
         
-        for i in nodeLst :
-            _plus = plus(i, n)
-            _dou = double_(i)
-            _tri = triple(i)
-            
-            if _plus < y :
-                newnodeLst.append(_plus)
-            elif _plus == y :
-                return answer
-            
-            if _dou < y :
-                newnodeLst.append(_dou)
-            elif _dou == y :
-                return answer
-            
-            if _tri < y :
-                newnodeLst.append(_tri)
-            elif _tri == y :
-                return answer
-            
-        if len(newnodeLst) == 0 :
-            return -1
+        while True :
+            if len(lst) > 0 and lst[0][0] < tmp :
+                val, idx = heappop(lst)
+                answer[idx] = tmp
+            else :
+                break
+                
+        heappush(lst, [tmp, i])
         
-        nodeLst = list(set(newnodeLst))
-    
     return answer
