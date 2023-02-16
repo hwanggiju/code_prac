ddd271786c05470e1163cba9d364902b15fe32e5
hwanggiju <qltjzj555@gmail.com>
2023-02-16 21:52:48 +0900

    프로그래머스 / 폰켓몬 - python

--- practice.py
+++ practice.py
@@ -1,43 +1,13 @@
-import sys
-
-sys.setrecursionlimit(10000)
-
-def bfs(maps, x, y, s) :
+# 폰켓몬 문제
+def solution(nums):
+    answer = 0
     
-    if x >= len(maps[0]) or y >= len(maps) or x < 0 or y < 0 :
-        return s
+    a = len(set(nums))
+    b = len(nums)/2
     
-    if maps[y][x] == "X" :
-        return s
-    
-    s += int(maps[y][x])
-    
-    maps[y][x] = "X"
-    
-    s = bfs(maps, x, y - 1, s)  # 상
-    s = bfs(maps, x, y + 1, s)  # 하
-    s = bfs(maps, x - 1, y, s)  # 좌
-    s = bfs(maps, x + 1, y, s)  # 우
-
-    return int(s)
-
-def solution(maps):
-    answer = []
-    map_ = []
-
-    for i in maps :  
-        map_.append(list(i))
-
-    for i in range(len(map_)) :
-        for j in range(len(map_[0])) :
-            if map_[i][j] == "X" :
-                pass
-            else :
-                answer.append(bfs(map_, j, i, 0))
-
-    if answer :
-        answer = sorted(answer)
+    if a >= b :
+        answer = b
     else :
-        answer.append(-1)
+        answer = a
     
     return answer
