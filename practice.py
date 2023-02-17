b8006298589140b61e0f7dfad5782354cc1a4f03
hwanggiju <qltjzj555@gmail.com>
2023-02-17 21:55:28 +0900

    프로그래머스 / 기능개발 - Python

--- practice.py
+++ practice.py
@@ -1,12 +1,26 @@
-# 같은 숫자는 싫어
-def solution(arr):
+# 기능개발
+import math
+def solution(progresses, speeds):
     answer = []
+    dayLst = []
+    q = []
     
-    for i in arr :
-        if len(answer) == 0 :
-            answer.append(i)
-            
-        elif len(answer) != 0 and answer[-1] != i :
-            answer.append(i)
+    for i, j in zip(progresses, speeds) :
+        tmp = 100 - i
+        day = math.ceil(tmp / j)
+        dayLst.append(day)
+    
+    q.append(dayLst[0])
+    
+    for i in range(1, len(dayLst)) :
+        if q[0] >= dayLst[i] :
+            q.append(dayLst[i])
+        elif q[0] < dayLst[i] :
+            answer.append(len(q))
+            q = []
+            q.append(dayLst[i])
+        
+        if i == (len(dayLst) - 1) :
+            answer.append(len(q))
             
     return answer
