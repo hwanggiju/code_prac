e3ff6fa42cd7fc256a5e4a7a82127e00d92b6101
hwanggiju <qltjzj555@gmail.com>
2023-03-16 21:20:23 +0900

    백준 - 구간 합 구하기 4

--- practice.py
+++ practice.py
@@ -1,15 +1,33 @@
-# 백준 평균 구하기
+# 백준 구간 합 구하기 4
 
 '''
-n 과목 수 입력받기
-score 리스트에 입력받은 점수 정보 저장하기
-max_ 변수에 점수 최대값 저장하기
-score 리스트 점수 총합 구하기
-평균값 계산식으로 출력하기
+수의 개수 n과 합을 구해야 하는 횟수 M 입력받기
+numLst 에 n개의 수 리스트에 저장
+sumArray 합 배열 선언
+temp 변수 선언
+
+for i in numLst :
+    temp 에 i 숫자 더하기
+    합 배열에 추가하기
+
+for i in range(M) ;
+    구간 합 적용해서 출력하기
 '''
 
-n = int(input())
-score = list(map(int, input().split()))
-max_ = max(score)
-sum_ = sum(score)
-print(sum_*100/max_/n)
+import sys
+
+# 그냥 input()은 sys.stdin.readline 보다 개행 문자를 삭제해서 리턴하기 때문에 속도가 느리다.
+# 반면 sys.stdin.readline는 개행 문자를 포함하여 출력하는 대신 속도가 빠른 편이다.
+input = sys.stdin.readline
+n, M = map(int, input().split())
+numLst = list(map(int, input().split()))
+sumArray = [0]
+temp = 0
+
+for i in numLst :
+    temp += i
+    sumArray.append(temp)
+    
+for i in range(M) :
+    start, end = map(int, input().split())
+    print(sumArray[end] - sumArray[start-1])
