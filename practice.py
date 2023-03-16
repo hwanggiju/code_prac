f0982827c51f5a926aa169d2e296e6621ee1615a
hwanggiju <qltjzj555@gmail.com>
2023-03-16 20:17:06 +0900

    백준 - 숫자 합 구하기

--- practice.py
+++ practice.py
@@ -1,10 +1,21 @@
-# 빅-오메가 표기법 시간 복잡도 = 1 번
-# 빅-세타 표기법 시간 복잡도 = N/2 번
-# 빅-오 표기법 시간 복잡도 = N 번
-import random
-findNumber = random.randrange(1, 101)   # 1~100 사이 랜덤값 생성
+# 백준 숫자 합 구하기
 
-for i in range(1, 101) :
-    if i == findNumber :
-        print(i)
-        break
+'''
+n값 받기
+공백 없는 숫자 입력값 받고, 리스트 자료구조로 저장하기
+결과값 변수 sum 선언
+
+for i in 리스트 :
+    sum에 리스트 각 원소 정수로 변환하여 더하기
+
+sum 출력
+'''
+
+n = input()
+number = list(input())
+sum = 0
+
+for i in number :
+    sum += int(i)
+    
+print(sum)
