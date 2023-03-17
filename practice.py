14e2c252b79b4a70ac7c078a51f8dde536b8ae32
hwanggiju <qltjzj555@gmail.com>
2023-03-17 22:26:23 +0900

    백준 - '좋은 수' 구하기

--- practice.py
+++ practice.py
@@ -1,22 +1,28 @@
-# 백준 주몽의 명령
+# 백준 '좋은 수' 구하기
 
 '''
-재료의 개수 n과 갑옷이 되는 번호 m 입력 받기
-재료 데이터 리스트에 저장하기 dataLst
-dataLst 오름차순 정렬
-시작점 인덱스 : startIndex = 0
-끝점 인덱스 : endIndex = n-1
-결과값 : count = 0
+수의 개수 n 입력받기
+n개의 데이터를 리스트에 저장 dataLst
+dataLst 오름차순으로 정렬
+결과값 변수 count = 0
+시작점 : startIndex = 0
+끝점 : endIndex = n-1
 
-while i < j :
-    if 재료합이 < M :
-        시작점 1 증가
-    elif 재료합이 > M :
-        끝점 1 감소
-    else :
-        결과값 1 증가
-        시작점 1 증가
-        끝점 1 감소
+for i in range(n) :
+    기준이 되는 값 dataLst[i]
+    while startIndex < endIndex :
+        if dataLst[startIndex] + dataLst[endIndex] == 기준값 :
+            if 시작점 != 기준값 and 끝점 != 기준값 :
+                결과값 1 증가
+                 break
+            if 시작점 == 기준값 :
+                시작점 인덱스 1 증가
+            if 끝점 == 기준값 :
+                끝점 인덱스 1 감소
+        elif dataLst[startIndex] + dataLst[endIndex] > 기준값 :
+            끝점 인덱스 1 감소
+        else : 
+            시작점 인덱스 1 증가
 
 결과값 출력
 '''
@@ -25,23 +31,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-m = int(input())
         
 dataLst = list(map(int, input().split(' ')))
 dataLst.sort()
 
-startIndex = 0
-endIndex = n-1
 count = 0
 
-while startIndex < endIndex :
-    if dataLst[startIndex] + dataLst[endIndex] < m :
-        startIndex += 1
-    elif dataLst[startIndex] + dataLst[endIndex] > m :
-        endIndex -= 1
-    else :
-        count += 1
-        startIndex += 1
-        endIndex -= 1
-
+for i in range(n) :
+    val = dataLst[i]
+    startIndex = 0
+    endIndex = n-1
+    while startIndex < endIndex :
+        if dataLst[startIndex] + dataLst[endIndex] == val :
+            if dataLst[startIndex] != val and dataLst[endIndex] != val :
+                count += 1
+                break
+            if dataLst[startIndex] == val :
+                startIndex += 1
+            if dataLst[endIndex] == val :
+                endIndex -= 1
+        elif dataLst[startIndex] + dataLst[endIndex] > val :
+            endIndex -= 1
+        else : startIndex += 1
+        
 print(count)
