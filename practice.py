0da84e30311fb2d5507803022625cbe465856c36
hwanggiju <qltjzj555@gmail.com>
2023-03-18 13:21:10 +0900

    백준 - 최솟값 찾기

--- practice.py
+++ practice.py
@@ -1,105 +1,33 @@
-# 백준 DNA 비밀번호
+# 백준 최솟값 찾기
 
 '''
-# 함수
-add_(c) :
-    새로운 값 더하고, check 업데이트
-    
-remove_(c) :
-    맨 앞의 원소 제거하고, check 업데이트
-    
-# 메인 코드
-문자열 크기 s와 부분 문자열 크기 p를 입력받기
-DNA 문자열 데이터 arr 입력받기
-A C G T 최소 개수 checkLst 입력받기
+데이터의 개수 n과 슬라이딩 윈도우 크기 L 입력받기
+덱 자료구조 dq 생성하기
+n개의 데이터 dataLst에 저장하기
 
-# 사용 변수 선언
-결과값 변수 result = 0
-비교 리스트 cmpLst
-check = 0 
-
-# 문제 해결 방법
-for i in range(4) :
-    checkLst에서 0인 원소 개수 만큼 check 값 증가 => 이유 : 최소 개수가 0이라는 것은 이미 만족되었다는 의미
-    
-for i in range(p) :
-    부분 문자열 길이 원소 cmpLst와 checkLst를 비교하고, 유효한 원소라면 check 값 증가
-    
-if check == 4 :
-    결과값 증가
-    
-for i in range(p, s) :
-    tmpIdx = i - p
-    add_(c) 새로운 원소 값 추가
-    remove_(c) 맨 앞의 원소 값 제거
-    if check == 4 :
-        결과값 증가
-
-결과값 출력
+for i in range(n) :
+    while dq and dq[-1][0] > dataLst[i] :
+        덱의 마지막 노드 데이터부터 현재 새로운 노드 데이터보다 큰 값은 제거
+    덱에 (데이터, 인덱스) 형태의 노드 추가
+    if len(dq) > L :
+        윈도우의 범위보다 덱의 길이가 크다면, 가장 앞의 노드를 제거
+    덱의 가장 앞 노드 데이터 최솟값 출력
 '''
 
-def add_(c) :
-    global checkLst, cmpLst, check
-    if c == 'A' :
-        cmpLst[0] += 1
-        if cmpLst[0] == checkLst[0] :
-            check += 1
-    elif c == 'C' :
-        cmpLst[1] += 1
-        if cmpLst[1] == checkLst[1] :
-            check += 1
-    if c == 'G' :
-        cmpLst[2] += 1
-        if cmpLst[2] == checkLst[2] :
-            check += 1
-    if c == 'T' :
-        cmpLst[3] += 1
-        if cmpLst[3] == checkLst[3] :
-            check += 1
-    
-def remove_(c) :
-    global checkLst, cmpLst, check
-    if c == 'A' :
-        if cmpLst[0] == checkLst[0] :
-            check -= 1
-        cmpLst[0] -= 1
-    elif c == 'C' :
-        if cmpLst[1] == checkLst[1] :
-            check -= 1
-        cmpLst[1] -= 1
-    elif c == 'G' :
-        if cmpLst[2] == checkLst[2] :
-            check -= 1
-        cmpLst[2] -= 1
-    elif c == 'T' :
-        if cmpLst[3] == checkLst[3] :
-            check -= 1
-        cmpLst[3] -= 1
+from collections import deque
+
+n, L = map(int, input().split())
+dq = deque()
+dataLst = list(map(int, input().split()))
+
+for i in range(n) :
+    while dq and dq[-1][0] > dataLst[i] :
+        dq.pop()
         
-
-s, p = map(int, input().split())
-arr = list(input())
-checkLst = list(map(int, input().split()))
-
-result = 0
-cmpLst = [0] * 4
-check = 0
-
-for i in range(4) :
-    if checkLst[i] == 0 :
-        check += 1
-
-for i in range(p) :
-    add_(arr[i])
+    dq.append((dataLst[i], i))
     
-if check == 4 :
-    result += 1
-    
-for i in range(p, s) :
-    tmpIdx = i - p
-    add_(arr[i])
-    remove_(arr[tmpIdx])
-    if check == 4 :
-        result += 1
+    if  dq[0][1] <= i-L :
+        dq.popleft()
         
-print(result)
+    print(dq[0][0], end=' ')
+    
