7adf8b9c5611ae7e412603b63748c86840a2bb2b
hwanggiju <qltjzj555@gmail.com>
2023-03-18 18:00:03 +0900

    백준 - 절대값 힙

--- practice.py
+++ practice.py
@@ -1,29 +1,39 @@
-# 백준 카드 2
+# 백준 절대값 힙
 
 '''
-카드 개수 n 입력받기
-큐 리스트 생성하기 q
+연산 개수 n 입력받기
+우선순위 큐 리스트 생성 pq
 
-for i in range(1, n+1) :
-    큐 리스트에 1부터 n까지 저장
-
-while len(q) > 1 :
-    맨 위의 카드 한 장을 버림
-    그 다음 카드 한 장을 맨 뒤로 보냄
-    
-큐에서 마지막 하나 남은 원소 출력
+for i in range(n) :
+    정수 입력 받기 val
+    if val == 0 :
+        if pq.empty() :
+            0 출력하기
+        else :
+            pq의 맨 앞의 원소 출력하기 (get())
+            
+    else :
+        pq의 새로운 기준으로 정렬하기 (put())
 '''
 
-from collections import deque
+from queue import PriorityQueue
+import sys
+
+print = sys.stdout.write
+input = sys.stdin.readline
+
 n = int(input())
+pq = PriorityQueue()
 
-q = deque()
-
-for i in range(1, n+1) :
-    q.append(i)
-    
-while len(q) > 1 :
-    q.popleft()
-    q.append(q.popleft())
-    
-print(q[0])
+for i in range(n) :
+    val = int(input())
+    if val == 0 :
+        if pq.empty() :
+            print('0\n')
+        else :
+            temp = pq.get() # 절대값 동일한 경우, (x, -x)로 저장되어 있다.
+            print(str(temp[1]) + '\n')
+    else :
+        # abs(val) 기준으로 먼저 정렬
+        # abs(val)이 같다면, val를 기준으로 정렬하여 양수 음수 판별 
+        pq.put((abs(val), val))
