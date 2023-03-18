7a7bc7df257a55e16fb8538dfd2305c9dd1b53b3
hwanggiju <qltjzj555@gmail.com>
2023-03-18 15:13:51 +0900

    백준 - 스택 수열

--- practice.py
+++ practice.py
@@ -1,33 +1,63 @@
-# 백준 최솟값 찾기
+# 백준 스택 수열
 
 '''
-데이터의 개수 n과 슬라이딩 윈도우 크기 L 입력받기
-덱 자료구조 dq 생성하기
-n개의 데이터 dataLst에 저장하기
+수열의 개수 n과 수열 리스트 arr 데이터 입력받기
+오름차순 자연수 num = 0
+stack 리스트 생성
+결과값 result 빈 문자열 생성
+정상적인 수열 확인 state = True
 
 for i in range(n) :
-    while dq and dq[-1][0] > dataLst[i] :
-        덱의 마지막 노드 데이터부터 현재 새로운 노드 데이터보다 큰 값은 제거
-    덱에 (데이터, 인덱스) 형태의 노드 추가
-    if len(dq) > L :
-        윈도우의 범위보다 덱의 길이가 크다면, 가장 앞의 노드를 제거
-    덱의 가장 앞 노드 데이터 최솟값 출력
+    val arr[i] 수열 데이터 저장
+    if val >= num :
+        while val >= num :
+            num을 stack에 추가
+            num 1 증가
+            result + 추가
+        마지막 원소 제거
+        result - 추가
+    else :
+        stack 마지막 원소 pop 후 temp에 저장
+        if temp != val :
+            print("NO")
+            state = False
+            break
+        else :
+            result - 추가
+
+if state :
+    결과값 출력
 '''
 
-from collections import deque
-
-n, L = map(int, input().split())
-dq = deque()
-dataLst = list(map(int, input().split()))
+n = int(input())
+arr = [0] * n
 
 for i in range(n) :
-    while dq and dq[-1][0] > dataLst[i] :
-        dq.pop()
-        
-    dq.append((dataLst[i], i))
+    arr[i] = int(input())
     
-    if  dq[0][1] <= i-L :
-        dq.popleft()
+stack = []
+result = ""
+state = True
+num = 1
+
+for i in range(n) :
+    val = arr[i]
+    if val >= num :
+        while val >= num :
+            stack.append(num)
+            num += 1
+            result += '+\n'
+        stack.pop()
+        result += '-\n'
         
-    print(dq[0][0], end=' ')
-    
+    else :
+        temp = stack.pop()
+        if temp != val :
+            print("NO")
+            state = False
+            break
+        else :
+            result += '-\n'
+
+if state :
+    print(result)
