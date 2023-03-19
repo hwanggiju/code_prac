d6bc43afa6ff192ec2c3f8f72904996150ef24e6
hwanggiju <qltjzj555@gmail.com>
2023-03-19 19:06:09 +0900

    합병 정렬 코드

--- practice.py
+++ practice.py
@@ -1,53 +1,34 @@
-import sys
-input = sys.stdin.readline
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-
-def quickSort(s, e, k) :
-    global a
-    if s < e :
-        pivot = partition(s, e)
-        if pivot == k :
-            return
-        elif k < pivot :
-            quickSort(s, pivot-1, k)
+def merge_sort(s, e) :
+    if e - s < 1 :
+        return
+    m = int(s + (e-s) / 2)
+    merge_sort(s, m)
+    merge_sort(m+1, e)
+    for i in range(s, e+1) :
+        tmp[i] = A[i]
+    k = s
+    idx1 = s
+    idx2 = m + 1
+    while idx1 <= m and idx2 <= e :
+        if tmp[idx1] > tmp[idx2] :
+            A[k] = tmp[idx2]
+            k += 1
+            idx2 += 1
         else :
-            quickSort(pivot+1, e, k)
-            
-def swap(i, j) :
-    global a
-    temp = a[i]
-    a[i] = a[j]
-    a[j] = temp
-    
-def partition(s, e) :
-    global a
-    
-    if s+1 == e :
-        if a[s] > a[e] :
-            swap(s, e)
-        return e
-    
-    m = (s+e) // 2
-    swap(s, m)
-    pivot = a[s]
-    i = s + 1
-    j = e
-    
-    while i <= j :
-        while pivot < a[j] and j > 0 :
-            j -= 1
-            
-        while pivot > a[i] and i < len(a) - 1 :
-            i += 1
-        if i <= j :
-            swap(i, j)
-            i += 1
-            j -= 1
-    
-    a[s] = a[j]
-    a[j] = pivot
-    return j
+            A[k] = tmp[idx1]
+            k += 1
+            idx1 += 1
+    while idx1 <= m :
+        A[k] = tmp[idx1]
+        k += 1
+        idx1 += 1
+    while idx2 <= e :
+        A[k] = tmp[idx2]
+        k += 1
+        idx2 += 1
+        
+tmp = [0] * 6
+A = [0, 5, 4, 3, 2, 1]
+merge_sort(1, 5)
 
-quickSort(0, n-1, k-1)
-print(a[k-1])
+print(A)
