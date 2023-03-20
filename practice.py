e2b4dad28f376be795e42b4b358881ffcff03b32
hwanggiju <qltjzj555@gmail.com>
2023-03-20 21:52:49 +0900

    백준 - 연결 요소의 개수 수정

--- practice.py
+++ practice.py
@@ -7,13 +7,14 @@
 
 def DFS() :
     visited에 노드 방문 기록
-    방문한 노드와 인접한 노드 중 방문하지 않은 노드 DFS 실행 (재귀 함수로 구현)
+    if not visited[i] :
+        방문한 노드와 인접한 노드 중 방문하지 않은 노드 DFS 실행 (재귀 함수로 구현)
     
 for i in range(m) :
     graph 인접 리스트 저장
     
 for i in range(1, n+1) :
-    if !visited[i] :
+    if not visited[i] :
         연결 요소 개수 1 증가
         DFS 실행
 
