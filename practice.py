6f65c8faaaa47b4c192eda99046fd26b1014846d
hwanggiju <qltjzj555@gmail.com>
2023-03-25 23:26:09 +0900

    백준 잃어버린 괄호 - 1541

--- practice.py
+++ practice.py
@@ -1,55 +1,34 @@
-# 백준 수 찾기 - 1920
+# 백준 잃어버린 괄호 - 1541
 
 '''
-데이터 개수 n 입력받기
-n개의 데이터 리스트 data 입력받고, 정렬하기
+정답 변수 answer 초기화
+입력받은 계산식 '-' 문자로 쪼개서 리스트로 저장하기
 
-찾을 데이터 개수 findData 입력받기
-찾을 데이터 리스트 findLst로 입력받고, 저장하기
-
-for i in findLst :
-    찾았는지 못찾았는지 find
-    시작 인덱스 start와 끝 인덱스 end 초기화
+for i in range(len(cal)) :
+    더하기 결과값 변수 result 초기화
+    '+' 문자로 쪼개서 tmp 리스트로 저장하기
+    for j in tmp :
+        result에 int(j) 누적하기
     
-    while start <= end :
-        중앙값 m 값 대입
-        if i < data[m] :
-            end 값 중앙값 이전 인덱스로 업데이트
-        elif i > data[m] :
-            start 값 중앙값 다음 인덱스로 업데이트
-        else :
-            find 찾았다!!
-            반복 종료
-    if find :
-        1 출력
+    if i == 0 :
+        가장 첫번째 숫자는 answer 그냥 더해주기
     else :
-        0 출력
+        나머지 숫자는 answer에 result 만큼 빼기
+
+answer 출력하기
 '''
 
-n = int(input())
+answer = 0
+cal = input().split('-')
 
-data = list(map(int, input().split()))
-data.sort()
+for i in range(len(cal)) :
+    result = 0
+    tmp = cal[i].split('+')
+    for j in tmp :
+        result += int(j)
+    if i == 0 :
+        answer += result
+    else : 
+        answer -= result
 
-findData = int(input())
-findLst = list(map(int, input().split()))
-
-for i in findLst :
-    find = False
-    start = 0
-    end = len(data) - 1
-    while  start <= end :
-        m = (start + end) // 2
-        if i < data[m] :
-            end = m - 1
-        elif i > data[m] :
-            start = m + 1
-        else :
-            find = True
-            break
-    if find :
-        print(1)
-    else :
-        print(0)
-            
-    
+print(answer)
