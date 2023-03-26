492024e094d7d51ca47a2f6e22076c13c9fed205
hwanggiju <qltjzj555@gmail.com>
2023-03-26 13:00:25 +0900

    백준 - 소수 구하기 1929

--- practice.py
+++ practice.py
@@ -1,34 +1,36 @@
-# 백준 잃어버린 괄호 - 1541
+# 백준 소수 구하기 1929
 
 '''
-정답 변수 answer 초기화
-입력받은 계산식 '-' 문자로 쪼개서 리스트로 저장하기
+m이상 n이하의 m, n 입력받기
+소수 탐색 리스트 primeLst 초기화하기
 
-for i in range(len(cal)) :
-    더하기 결과값 변수 result 초기화
-    '+' 문자로 쪼개서 tmp 리스트로 저장하기
-    for j in tmp :
-        result에 int(j) 누적하기
-    
-    if i == 0 :
-        가장 첫번째 숫자는 answer 그냥 더해주기
-    else :
-        나머지 숫자는 answer에 result 만큼 빼기
+for i in range(2, n+1) :
+    primeLst에 순서대로 데이터 업데이트
 
-answer 출력하기
+for i in range(2, int(math.sqrt(n)) + 1) : # 2부터 제곱근 n 이하까지 탐색하기
+    if primeLst[i] == 0 :
+        continue
+    for j in range(i+i, n+1, i) :
+        primeLst[i] 배수 삭제하기
+
+for i in range(m, n+1) :
+    primeLst[i] 남은 소수 차례대로 출력하기
 '''
 
-answer = 0
-cal = input().split('-')
+import math
 
-for i in range(len(cal)) :
-    result = 0
-    tmp = cal[i].split('+')
-    for j in tmp :
-        result += int(j)
-    if i == 0 :
-        answer += result
-    else : 
-        answer -= result
+m, n = map(int, input().split())
+primeLst = [0] * (n+1)
 
-print(answer)
+for i in range(2, n+1) :
+    primeLst[i] = i
+
+for i in range(2, int(math.sqrt(n)) + 1) :
+    if primeLst[i] == 0 :
+        continue
+    for j in range(i+i, n+1, i) :
+        primeLst[j] = 0
+
+for i in range(m, n+1) :  
+    if primeLst[i] != 0 :
+        print(primeLst[i])
