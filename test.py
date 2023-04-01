ac045c431c321d1c6090a8f90cb56b428c815f72
hwanggiju <qltjzj555@gmail.com>
2023-04-01 21:31:12 +0900

    코드업 - 리모컨 3120

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
-pasta = []
-juice = []
+nowTemp, goalTemp = map(int, input().split())
 
-for i in range(5) :
-    val = int(input())
-    if i < 3:
-        pasta.append(val)
-    else :
-        juice.append(val)
+cnt = 0
+tmp = 0     # 몫
+val = abs(nowTemp - goalTemp)
 
-result = []
-for p in pasta :
-    for j in juice :
-        tmp = (p+j) + (p + j) * 0.1
-        result.append(tmp)
-        
-print(min(result))
-        
+if val >= 10 :
+    tmp = val // 10
+val = val % 10
+cnt += tmp
+
+if val >= 8 :
+    val -= 10
+    val = abs(val) 
+    cnt += 1
+
+if val >= 5 :
+    tmp = val // 5
+    val = val % 5
+    cnt += tmp
+
+if val >= 3 :
+    val -= 5
+    val = abs(val)
+    cnt += 1
+
+if val >= 1 :
+    tmp = val // 1
+    cnt += tmp
+
+    
+print(cnt)
     
