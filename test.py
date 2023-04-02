91c17de6fe0c09c2f65736aee4dba5c1b7fd7b62
hwanggiju <qltjzj555@gmail.com>
2023-04-02 12:34:06 +0900

    코드업 펜션 4040

--- test.py
+++ test.py
@@ -1,20 +1,51 @@
-n = int(input()) # 토핑의 종류 수
-a, b = map(int, input().split()) # 도우의 가격, 토핑의 가격
-c = int(input()) # 도우의 칼로리
-n_cal = [] # 토핑 칼로리 리스트
-result_cal = [] # 최고의 피자 리스트
-for _ in range(n) :
-    cal = int(input())
-    n_cal.append(cal)
+n, m = map(int, input().split())
+room = [['X'] * (m+2) for _ in range(n+2)]
 
-n_cal.sort(reverse=True)
-
-money = a
-result = c
-
-for i in range(n) :
-    money += b
-    result += n_cal[i]
-    result_cal.append((result // money))
+for i in range(1, n+1) :
+    reserve = list(input())
+    for j in range(1, m+1) :
+        room[i][j] = reserve[j-1]
     
-print(max(result_cal))
+start, end = map(int, input().split())
+
+check = [[0] * (m+2) for _ in range(n+2)]
+
+def func(x, y, v) :
+    if room[x][y] == 'O' :
+        check[x][y] = func(x+1, y, v+1)
+        return check[x][y]
+    else :
+        check[x][y] = 0
+        return v
+    
+for i in range(1, n+1) :
+    for j in range(1, m+1) :
+        func(i, j, 0)
+
+cnt = 0
+noReverse = False
+memory = False
+
+for i in range(start, end) :
+    if 'O' not in room[i] :
+        noReverse = True
+
+while not noReverse and start < end :
+    
+    start += max(check[start])
+    
+    if cnt == 0 and start >= end :
+        memory = True
+        break
+    elif start < end :
+        cnt += 1
+  
+if noReverse :
+    print(-1)
+else :
+    if memory :
+        print(0)
+    else :
+        print(cnt)
+
+        
