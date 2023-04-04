5d22382fbf3cbdef5261fda7f96def0ecf8a6257
hwanggiju <qltjzj555@gmail.com>
2023-04-04 22:55:06 +0900

    코드업 자물쇠 풀이중

--- test.py
+++ test.py
@@ -1,51 +1,52 @@
-n, m = map(int, input().split())
-room = [['X'] * (m+2) for _ in range(n+2)]
+from collections import deque
 
-for i in range(1, n+1) :
-    reserve = list(input())
-    for j in range(1, m+1) :
-        room[i][j] = reserve[j-1]
-    
-start, end = map(int, input().split())
+n = int(input())
+lock = list(map(int, input().split()))
+firstLock = list(range(1,n+1))
 
-check = [[0] * (m+2) for _ in range(n+2)]
+rangeInx = []
 
-def func(x, y, v) :
-    if room[x][y] == 'O' :
-        check[x][y] = func(x+1, y, v+1)
-        return check[x][y]
-    else :
-        check[x][y] = 0
-        return v
-    
-for i in range(1, n+1) :
-    for j in range(1, m+1) :
-        func(i, j, 0)
-
-cnt = 0
-noReverse = False
-memory = False
-
-for i in range(start, end) :
-    if 'O' not in room[i] :
-        noReverse = True
-
-while not noReverse and start < end :
-    
-    start += max(check[start])
-    
-    if cnt == 0 and start >= end :
-        memory = True
-        break
-    elif start < end :
-        cnt += 1
-  
-if noReverse :
-    print(-1)
-else :
-    if memory :
-        print(0)
-    else :
-        print(cnt)
-
+def k_shift(k) :
+    for i in range(k) :
+        shiftVal = tmpLock.pop()
+        tmpLock.insert(0, shiftVal)
+    return tmpLock
         
+def reverseRange(s, e) :
+    tmp = tmpLock[s:e]
+    
+    for i in range(s, e) :
+        val = tmp.pop()
+        tmpLock[i] = val
+    return tmpLock
+
+# 뒤집기 구간 구하는 곳
+for i in range(n-1) :
+    diff = lock[i] - lock[i+1]
+    if diff > 1 :
+        rangeInx.append(i+1)
+
+for i in range(1, n+1) :     # 첫번째 오른쪽 밀기
+    j = 0
+    s, e = rangeInx
+    while j < 10 :
+        if e+j >= n :
+            break
+        
+        tmpLock = lock.copy()
+        # print('backup', lock)
+        j += 1
+        tmpLock = k_shift(j)
+        # print('first', tmpLock)
+        reverseRange(s+j, e+j)
+        k_shift(i)
+        # print('second', tmpLock)
+        
+        if tmpLock == firstLock :
+            break
+    if tmpLock == firstLock :
+            break
+
+print(i+1)
+print(s+j, e+j-1)
+print(j-1)
